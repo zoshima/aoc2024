25e73699853a756c34cbff28ffd3d6d28be94143
zoshima <dmykras@gmail.com>
2024-12-03 08:54:12 +0100

    add template

---
+++ day0/main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	println("part1", part1("input.txt"))
+	println("part2", part2("input.txt"))
+}
+
+func part1(input string) int {
+	return 0
+}
+
+func part2(input string) int {
+	return 0
+}
+
+func loadInput(path string) any {
+	return nil
+}

7b976e6aad92051860470c0d327bb25f8fb42518
zoshima <dmykras@gmail.com>
2024-12-04 11:53:40 +0100

    day 4, part 1

---
+++ day4/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+type Position struct {
+	X int
+	Y int
+}
+
+func main() {
+	println("part1", part1("input.txt"))
+	println("part2", part2("input.txt"))
+}
+
+func part1(fp string) int {
+	result := 0
+
+	input := loadInput(fp)
+
+	for y := range input {
+		for x := range input[y] {
+			if input[y][x] != 'X' {
+				continue
+			}
+
+			// north
+			if y >= 3 &&
+				input[y-1][x] == 'M' &&
+				input[y-2][x] == 'A' &&
+				input[y-3][x] == 'S' {
+				result++
+			}
+
+			// northwest
+			if y >= 3 && x >= 3 &&
+				input[y-1][x-1] == 'M' &&
+				input[y-2][x-2] == 'A' &&
+				input[y-3][x-3] == 'S' {
+				result++
+			}
+
+			// west
+			if x >= 3 &&
+				input[y][x-1] == 'M' &&
+				input[y][x-2] == 'A' &&
+				input[y][x-3] == 'S' {
+				result++
+			}
+
+			// southwest
+			if y < len(input)-3 && x >= 3 &&
+				input[y+1][x-1] == 'M' &&
+				input[y+2][x-2] == 'A' &&
+				input[y+3][x-3] == 'S' {
+				result++
+			}
+
+			// south
+			if y < len(input)-3 &&
+				input[y+1][x] == 'M' &&
+				input[y+2][x] == 'A' &&
+				input[y+3][x] == 'S' {
+				result++
+			}
+
+			// southeast
+			if y < len(input)-3 && x < len(input[y])-3 &&
+				input[y+1][x+1] == 'M' &&
+				input[y+2][x+2] == 'A' &&
+				input[y+3][x+3] == 'S' {
+				result++
+			}
+
+			// east
+			if x < len(input[y])-3 &&
+				input[y][x+1] == 'M' &&
+				input[y][x+2] == 'A' &&
+				input[y][x+3] == 'S' {
+				result++
+			}
+
+			// northeast
+			if y >= 3 && x < len(input[y])-3 &&
+				input[y-1][x+1] == 'M' &&
+				input[y-2][x+2] == 'A' &&
+				input[y-3][x+3] == 'S' {
+				result++
+			}
+		}
+	}
+
+	return result
+}
+
+func part2(fp string) int {
+	return 0
+}
+
+func loadInput(fp string) [][]rune {
+	file, _ := os.Open(fp)
+	defer file.Close()
+
+	input := make([][]rune, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		input = append(input, []rune(line))
+	}
+
+	return input
+}

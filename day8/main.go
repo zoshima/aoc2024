4613b2a51fa358847a7557b58b46b2fe3e511934
zoshima <dmykras@gmail.com>
2024-12-08 15:41:00 +0100

    day 8, part 2

--- day8/main.go
+++ day8/main.go
@@ -12,10 +12,79 @@ func main() {
 
 func part1(fp string) int {
 	input := loadInput(fp)
+	antennas := getAntennas(input)
 	result := 0
 
+	for _, positions := range antennas {
+		for i := 0; i < len(positions)-1; i++ {
+			for j := i + 1; j < len(positions); j++ {
+				y1, x1 := positions[i][0], positions[i][1]
+				y2, x2 := positions[j][0], positions[j][1]
+
+				dy, dx := y2-y1, x2-x1
+
+				y, x := y1-dy, x1-dx
+				if setAntinode(input, y, x) == 1 {
+					result++
+				}
+
+				y, x = y2+dy, x2+dx
+				if setAntinode(input, y, x) == 1 {
+					result++
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func part2(fp string) int {
+	input := loadInput(fp)
+	antennas := getAntennas(input)
+	result := 0
+
+	for _, positions := range antennas {
+		for i := 0; i < len(positions)-1; i++ {
+			for j := i + 1; j < len(positions); j++ {
+				y1, x1 := positions[i][0], positions[i][1]
+				y2, x2 := positions[j][0], positions[j][1]
+
+				dy, dx := y2-y1, x2-x1
+
+				y, x := y1, x1
+				for {
+					exitCode := setAntinode(input, y, x)
+					if exitCode == 0 {
+						break
+					} else if exitCode == 1 {
+						result++
+					}
+
+					y, x = y-dy, x-dx
+				}
+
+				y, x = y2, x2
+				for {
+					exitCode := setAntinode(input, y, x)
+					if exitCode == 0 {
+						break
+					} else if exitCode == 1 {
+						result++
+					}
+
+					y, x = y+dy, x+dx
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func getAntennas(m [][]rune) map[rune][][2]int {
 	antennas := make(map[rune][][2]int)
-	for y, line := range input {
+	for y, line := range m {
 		for x, r := range line {
 			if r == '.' {
 				continue
@@ -29,42 +98,19 @@ func part1(fp string) int {
 		}
 	}
 
-	setAntinode := func(y, x int) bool {
-		if y >= 0 && y < len(input) && x >= 0 && x < len(input[y]) {
-			if input[y][x] != '#' {
-				input[y][x] = '#'
-				return true
-			}
-		}
-
-		return false
-	}
-
-	for _, positions := range antennas {
-		for i := 0; i < len(positions)-1; i++ {
-			for j := i + 1; j < len(positions); j++ {
-				y1, x1 := positions[i][0], positions[i][1]
-				y2, x2 := positions[j][0], positions[j][1]
-
-				dy, dx := y2-y1, x2-x1
-
-				y, x := y1-dy, x1-dx
-				if setAntinode(y, x) {
-					result++
-				}
-
-				y, x = y2+dy, x2+dx
-				if setAntinode(y, x) {
-					result++
-				}
-			}
-		}
-	}
-
-	return result
+	return antennas
 }
 
-func part2(input any) int {
+func setAntinode(m [][]rune, x, y int) int {
+	if y >= 0 && y < len(m) && x >= 0 && x < len(m[y]) {
+		if m[y][x] != '#' {
+			m[y][x] = '#'
+			return 1
+		}
+
+		return 2
+	}
+
 	return 0
 }
 

023d3c2a0289bbb3b41e2de82b178a565d1d6a2d
zoshima <dmykras@gmail.com>
2024-12-10 17:45:06 +0100

    day 10, part 1

---
+++ day10/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	println("part1", part1("input.txt"))
+	println("part2", part2("input.txt"))
+}
+
+func part1(fp string) int {
+	result := 0
+
+	input := loadInput(fp)
+	for y := range input {
+		for x := range input[y] {
+			if input[y][x] != 0 {
+				continue
+			}
+
+			peaks := make(map[string]any)
+			traverse(input, peaks, x, y, 0)
+			result += len(peaks)
+		}
+	}
+
+	return result
+}
+
+func part2(fp string) int {
+	return 0
+}
+
+func traverse(m [][]int, peaks map[string]any, x, y, height int) {
+	if y < 0 || y >= len(m) || x < 0 || x >= len(m[y]) {
+		return
+	}
+
+	if m[y][x] != height {
+		return
+	}
+
+	if height == 9 {
+		key := fmt.Sprintf("%d:%d", y, x)
+		peaks[key] = nil
+		return
+	}
+
+	traverse(m, peaks, x, y-1, height+1) // north
+	traverse(m, peaks, x-1, y, height+1) // west
+	traverse(m, peaks, x, y+1, height+1) // south
+	traverse(m, peaks, x+1, y, height+1) // east
+}
+
+func loadInput(fp string) [][]int {
+	file, _ := os.Open(fp)
+	defer file.Close()
+
+	input := make([][]int, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+
+		row := make([]int, len(line))
+		for i := range line {
+			row[i] = int(line[i] - '0')
+		}
+
+		input = append(input, row)
+	}
+
+	return input
+}

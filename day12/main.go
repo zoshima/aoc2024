5dbc6713bb6b77f02faee666e34382556bdd6143
zoshima <dmykras@gmail.com>
2024-12-13 00:09:42 +0100

    day 12, part 2 (unf)

--- day12/main.go
+++ day12/main.go
@@ -14,6 +14,27 @@ func part1(fp string) int {
 	input := loadInput(fp)
 	result := 0
 
+	areas, perimeters, _ := solve(input)
+	for i := range areas {
+		result += areas[i] * perimeters[i]
+	}
+
+	return result
+}
+
+func part2(fp string) int {
+	input := loadInput(fp)
+	result := 0
+
+	areas, _, sides := solve(input)
+	for i := range areas {
+		result += areas[i] * sides[i]
+	}
+
+	return result
+}
+
+func solve(input [][]rune) (areas, perimeters, sides []int) {
 	for y := 0; y < len(input); y++ {
 		for x := 0; x < len(input[y]); x++ {
 			s := input[y][x]
@@ -21,32 +42,32 @@ func part1(fp string) int {
 				continue
 			}
 
-			area, perimeter := traverse(input, s, x, y)
-			result += area * perimeter
+			mark := rune(s + 32)
+			area, perimeter, side := traverse(input, s, mark, x, y)
+
+			areas = append(areas, area)
+			perimeters = append(perimeters, perimeter)
+			sides = append(sides, side)
 		}
 	}
 
-	return result
+	return
 }
 
-func part2(fp string) int {
-	return 0
-}
-
-func traverse(input [][]rune, symbol rune, x, y int) (area, perimeter int) {
+func traverse(input [][]rune, symbol rune, mark rune, x, y int) (area, perimeter, sides int) {
 	if y < 0 || x < 0 || y >= len(input) || x >= len(input[y]) {
-		return 0, 1
+		return 0, 1, 0
 	}
 
 	if input[y][x] != symbol {
-		if input[y][x] == symbol+32 {
-			return 0, 0
+		if input[y][x] == mark {
+			return 0, 0, 0
 		}
 
-		return 0, 1
+		return 0, 1, 0
 	}
 
-	input[y][x] = symbol + 32
+	input[y][x] = mark
 	area = 1
 	perimeter = 0
 
@@ -58,12 +79,48 @@ func traverse(input [][]rune, symbol rune, x, y int) (area, perimeter int) {
 	}
 
 	for _, dir := range dirs {
-		a, p := traverse(input, symbol, dir[0], dir[1])
+		a, p, s := traverse(input, symbol, mark, dir[0], dir[1])
 		area += a
 		perimeter += p
+		sides += s
 	}
 
+	sides += countCorners(input, x, y)
+
 	return
+}
+
+func countCorners(input [][]rune, x, y int) int {
+	result := 0
+
+	within := func(px, py int) bool {
+		if px < 0 || py < 0 || py >= len(input) || px >= len(input[py]) {
+			return false
+		}
+
+		return input[py][px] == input[y][x]
+	}
+
+	checks := [][3]bool{
+		{within(x-1, y-1), within(x-1, y), within(x, y-1)}, // NW
+		{within(x+1, y-1), within(x+1, y), within(x, y-1)}, // NE
+		{within(x-1, y+1), within(x-1, y), within(x, y+1)}, // SW
+		{within(x+1, y+1), within(x+1, y), within(x, y+1)}, // SE
+	}
+
+	for _, check := range checks {
+		if check[1] != check[2] {
+			continue
+		}
+
+		if check[0] == false {
+			result++
+		} else if check[1] == false && check[2] == false {
+			result++
+		}
+	}
+
+	return result
 }
 
 func loadInput(fp string) [][]rune {
--- day12/main_test.go
+++ day12/main_test.go
@@ -12,8 +12,22 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
-	want := 0
+	want := 1206
 	got := part2("input_test.txt")
+
+	if want != got {
+		t.Fatalf("want %d, got %d", want, got)
+	}
+
+	want = 236
+	got = part2("input_test_2.txt")
+
+	if want != got {
+		t.Fatalf("want %d, got %d", want, got)
+	}
+
+	want = 368
+	got = part2("input_test_3.txt")
 
 	if want != got {
 		t.Fatalf("want %d, got %d", want, got)
